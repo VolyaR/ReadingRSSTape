2346bc4ac0e73226ef5141cb36863a93e26bc362
Olga Rudaya <volya.rudaya@gmail.com>
2019-03-14 23:01:58 +0300

    Create WebApi

---
+++ RssReaderConsole/WebAPI/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Mvc;
+
+using Services;
+
+namespace WebAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public JsonResult AllNews()
+        {
+            var reader = new DataBaseReader();
+            var news = reader.ReadAllNews();
+            return Json(news, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public JsonResult HabrNews()
+        {
+            var reader = new DataBaseReader();
+            var news = reader.ReadHabrNews();
+            return Json(news, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public JsonResult ReadInterfaxNews()
+        {
+            var reader = new DataBaseReader();
+            var news = reader.ReadInterfaxNews();
+            return Json(news, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
