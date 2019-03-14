a5e5f86e8d7e0d30e129baa8b6cc718cca59394d
Olga Rudaya <volya.rudaya@gmail.com>
2019-03-15 00:36:06 +0300

    Create UI

--- RssReaderConsole/WebAPI/Controllers/HomeController.cs
+++ RssReaderConsole/WebAPI/Controllers/HomeController.cs
@@ -1,33 +1,14 @@
 ﻿using System.Web.Mvc;
 
-using Services;
-
 namespace WebAPI.Controllers
 {
     public class HomeController : Controller
     {
-        [HttpGet]
-        public JsonResult AllNews()
+        public ActionResult Index()
         {
-            var reader = new DataBaseReader();
-            var news = reader.ReadAllNews();
-            return Json(news, JsonRequestBehavior.AllowGet);
-        }
+            ViewBag.Title = "Home Page";
 
-        [HttpGet]
-        public JsonResult HabrNews()
-        {
-            var reader = new DataBaseReader();
-            var news = reader.ReadHabrNews();
-            return Json(news, JsonRequestBehavior.AllowGet);
-        }
-
-        [HttpGet]
-        public JsonResult ReadInterfaxNews()
-        {
-            var reader = new DataBaseReader();
-            var news = reader.ReadInterfaxNews();
-            return Json(news, JsonRequestBehavior.AllowGet);
+            return View();
         }
     }
 }
---
+++ RssReaderConsole/WebAPI/Controllers/NewsController.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Mvc;
+
+using Services;
+
+namespace WebAPI.Controllers
+{
+    public class NewsController : Controller
+    {
+        public JsonResult All()
+        {
+            var reader = new DataBaseReader();
+            var news = reader.ReadAllNews();
+            return Json(news, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult Habr()
+        {
+            var reader = new DataBaseReader();
+            var news = reader.ReadHabrNews();
+            return Json(news, JsonRequestBehavior.AllowGet);
+        }
+        
+        public JsonResult Interfax()
+        {
+            var reader = new DataBaseReader();
+            var news = reader.ReadInterfaxNews();
+            return Json(news, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
